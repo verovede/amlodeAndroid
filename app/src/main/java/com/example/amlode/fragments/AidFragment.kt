027d6e3e987d4d24ef5d60dc004b0248fdc3cc55
Camila Belen Capua <camilabelencapua@gmail.com>
2022-10-09 13:40:56 -0300

    Se agrega logica para ir a la llamada del 911

--- app/src/main/java/com/example/amlode/fragments/AidFragment.kt
+++ app/src/main/java/com/example/amlode/fragments/AidFragment.kt
@@ -1,5 +1,6 @@
 package com.example.amlode.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,11 +9,15 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.Navigation
 import com.example.amlode.R
+import org.w3c.dom.Text
+import android.net.Uri;
 
 
 class AidFragment : Fragment() {
     lateinit var viewFragment: View
     lateinit var buttonBack: Button
+    lateinit var buttonCall: Button
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,11 +26,20 @@ class AidFragment : Fragment() {
         // Inflate the layout for this fragment
         viewFragment = inflater.inflate(R.layout.fragment_aid, container, false)
         buttonBack = viewFragment.findViewById<Button>(R.id.button_primeros_aux)
+        buttonCall = viewFragment.findViewById<Button>(R.id.button2)
+
         return viewFragment
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
     override fun onStart() {
         super.onStart()
+        buttonCall.setOnClickListener(View.OnClickListener() {
+            startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel: 911")));
+        });
     }
-
 }
+
