9c2c84d7855e4deebff93f99633d0ac574c6490c
Camila Belen Capua <camilabelencapua@gmail.com>
2022-10-15 17:34:04 -0300

    El boton logout está en usuario y en los deas hay boton Agregar dea

--- app/src/main/java/com/example/amlode/DeaActivity.kt
+++ app/src/main/java/com/example/amlode/DeaActivity.kt
@@ -9,11 +9,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.fragment_user.*
 
 class DeaActivity : AppCompatActivity() {
     lateinit var mGoogleSignInClient: GoogleSignInClient
-    lateinit var logout_google: Button
+    lateinit var add_dea: Button
 
 
     private val auth by lazy {
@@ -23,24 +22,6 @@ class DeaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dea)
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-        logout_google = findViewById(R.id.logout_google)
-
-        logout_google.setOnClickListener {
-            mGoogleSignInClient.signOut().addOnCompleteListener {
-                val intent = Intent(this, MainActivity::class.java)
-                Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
-                SavedPreference.setEmail(this, "")
-                SavedPreference.setUsername(this, "")
-                startActivity(intent)
-                finish()
-            }
-        }
+        add_dea = findViewById(R.id.add_dea)
     }
-
 }
--- app/src/main/java/com/example/amlode/fragments/UserFragment.kt
+++ app/src/main/java/com/example/amlode/fragments/UserFragment.kt
@@ -1,14 +1,21 @@
 package com.example.amlode.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.amlode.MainActivity
 import com.example.amlode.R
 import com.example.amlode.SavedPreference
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.squareup.picasso.Picasso
 
 class UserFragment : Fragment() {
@@ -17,6 +24,8 @@ class UserFragment : Fragment() {
     lateinit var username: TextView
     lateinit var email: TextView
     lateinit var photo: ImageView
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    lateinit var logout_user: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +35,7 @@ class UserFragment : Fragment() {
         username = viewFragment.findViewById(R.id.username)
         email = viewFragment.findViewById(R.id.email)
         photo =  viewFragment.findViewById(R.id.photo)
+        logout_user = viewFragment.findViewById(R.id.logout_user)
         return viewFragment
     }
 
@@ -34,5 +44,21 @@ class UserFragment : Fragment() {
         username.text = SavedPreference.getSharedPreference(context)?.getString("username","")
         email.text = SavedPreference.getSharedPreference(context)?.getString("email","")
         Picasso.with(context).load(SavedPreference.getPhoto()).into(photo)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+        logout_user.setOnClickListener {
+            mGoogleSignInClient.signOut().addOnCompleteListener {
+                val intent = Intent(context, MainActivity::class.java)
+                Toast.makeText(context, "Logging Out", Toast.LENGTH_SHORT).show()
+                SavedPreference.setEmail(requireContext(), "")
+                SavedPreference.setUsername(requireContext(), "")
+                startActivity(intent)
+            }
+        }
     }
 }
