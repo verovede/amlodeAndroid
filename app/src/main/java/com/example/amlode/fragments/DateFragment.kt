0e4d2111c7729dda1476ed8d6833a895dc5e3431
Camila Belen Capua <camilabelencapua@gmail.com>
2022-10-23 11:10:01 -0300

    Validación de 18 años, si es menor, no aparece el botón

--- app/src/main/java/com/example/amlode/fragments/DateFragment.kt
+++ app/src/main/java/com/example/amlode/fragments/DateFragment.kt
@@ -2,6 +2,7 @@ package com.example.amlode.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,8 @@ import com.example.amlode.LoginScreen
 import com.example.amlode.MainActivity.Companion.prefs
 import com.example.amlode.R
 import com.example.amlode.SplashActivity
+import java.time.LocalDate
+import java.time.Period
 
 class DateFragment : AppCompatActivity() {
     lateinit var register_date: Button
@@ -42,6 +45,18 @@ class DateFragment : AppCompatActivity() {
     private fun onDateSelected(day: Int, month: Int, year: Int) {
         date.setText("$day/$month/$year")
         prefs.saveDate("$day/$month/$year")
+
+        val eighteenYearsAgo = LocalDate.now() - Period.ofYears(18)
+        val pickedDate = LocalDate.of(year, month + 1, day)
+
+        if (pickedDate < eighteenYearsAgo) {
+            register_date.setVisibility(View.VISIBLE);
+            register_date.setOnClickListener {
+                val intent = Intent(this, LoginScreen::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
 
