d904382cf452932f42b7d71200a36edac9afc953
Camila Belen Capua <camilabelencapua@gmail.com>
2022-10-30 14:45:38 -0300

    La fecha minima es 1922 no 1900

--- app/src/main/java/com/example/amlode/DatePickerFragment.kt
+++ app/src/main/java/com/example/amlode/DatePickerFragment.kt
@@ -16,11 +16,18 @@ class DatePickerFragment(val listener: (day: Int, month: Int, year: Int) -> Unit
         var month = calendar.get(Calendar.MONTH)
         var day = calendar.get(Calendar.DAY_OF_MONTH)
         var datePicker = DatePickerDialog(requireContext(),R.style.DatePickerTheme, this, year, month, day)
-        datePicker.getDatePicker().setMaxDate(calendar.timeInMillis)
+        limitDate(calendar,year,datePicker)
         return datePicker
     }
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
         listener(day, month, year)
+    }
+
+    private fun limitDate(calendar: Calendar, year: Int, datePicker: DatePickerDialog){
+        calendar.set(Calendar.YEAR, year - 100)
+        datePicker.datePicker.setMinDate(calendar.timeInMillis)
+        calendar.set(Calendar.YEAR, year + 0)
+        datePicker.getDatePicker().setMaxDate(calendar.timeInMillis)
     }
 }
